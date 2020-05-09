71fba781de91cae2a8fa6c821b524bc3bf140dc0
王立广 <wangliguang@daling.com>
2020-05-09 18:11:03 +0800

    输入框

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Input } from 'antd';
+const { TextArea } = Input;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextArea
+          style={{ width: 400, height: 200, resize: 'none'}}
+          placeholder="to someone"
+          autoSize={false}
+        />
     </div>
   );
 }

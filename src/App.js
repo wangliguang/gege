0958ed602ed7c1620ae7fb217e22d8b40b818d66
王立广 <wangliguang@daling.com>
2020-08-01 10:57:57 +0800

    👍 FEAT: 输入框完事

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Input } from 'antd';
 const { TextArea } = Input;
 
-function App() {
-  return (
-    <div className="App">
-      <TextArea
-          style={{ width: 400, height: 200, resize: 'none'}}
-          placeholder="to someone"
-          autoSize={false}
-        />
-    </div>
-  );
-}
+export default class extends Component {
 
-export default App;
+    state = {
+        inputValue: '',
+    }
+
+    changeText = (e) => {
+        this.setState({
+            inputValue: e.currentTarget.innerHTML,
+        });
+    }
+
+    submit = () => {
+        console.log('XXXX', this.state.inputValue);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="title" >有时，不是不想说，不是没人说，而是想没有压力的说，如果这里能承载你的部分宣泄，便是我的幸运</div>
+
+                <div>
+                    
+                </div>
+    
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <TextArea
+                        onChange={this.changeText}
+                        style={{ width: '300px', height: '100px', resize: 'none'}}
+                        placeholder="to someone"
+                    />
+                    <button onClick={this.submit} className="submit">发送</button>
+                </div>
+            
+            
+            </div>
+        );
+    }
+}

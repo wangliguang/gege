ce264fd90bd6211d6e69e5e02e94bec7d1ed633a
王立广 <wangliguang@kaikeba.com>
2020-08-28 15:31:49 +0800

    ✨ 兼容手机

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ const { TextArea } = Input;
 const Bmob = require("hydrogen-js-sdk");
 Bmob.initialize('7896ca43470dec5e', '123456');
 
-document.title = "无人知晓的心声";
-
 export default class extends Component {
     allData = [];
     state = {
@@ -21,7 +19,7 @@ export default class extends Component {
 
     get100Message = async () => {
         const query = Bmob.Query("t_message");
-        const beginTime = new Date('2020-08-01 00:00:00').getTime();
+        const beginTime = 1596211200000; // 2020-08-01 00:00:00
         const curTime = new Date().getTime();
 
         const randomTime = Math.floor(Math.random() * (curTime - beginTime) ) + beginTime;
@@ -35,7 +33,6 @@ export default class extends Component {
 
     getNextMessage = () => {
         const index = Math.floor(Math.random() * this.allData.length);
-
         this.setState({
             message: this.allData[index].message,
         });
@@ -54,13 +51,21 @@ export default class extends Component {
         alert('发送成功');
     }
 
-    time(time = +new Date()) {
-        var date = new Date(time + 8 * 3600 * 1000);
-        return date.toJSON().substr(0, 19).replace('T', ' ').replace(/-/g, '-');
+    time(time) {
+        function add0(m){return m<10?'0'+m:m }
+        var  date = new Date(time);
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        var D = add0(date.getDate()) + ' ';
+        var h = add0(date.getHours()) + ':';
+        var m = add0(date.getMinutes()) + ':';
+        var s = add0(date.getSeconds());
+        
+        return Y+M+D+h+m+s;
     }
 
     changeText = (e) => {
-        
+        // 2020-08-6 11:49:7
         if(e && e.target && e.target.value){
             let value = e.target.value;
             console.log(value);
@@ -77,9 +82,9 @@ export default class extends Component {
         return (
             <div className="App">
                 <div className="title" >
-                    <div style={{height: 72 }}>不知从什么时候开始，想聊些心里话，貌似已经找不到没有合适的渠道。找家人担心家人操心；找朋友担心打扰人家；发圈要检查屏蔽某些人；发微博担心有人抬杠；听音乐有感而发也被调侃网抑云</div>
-                    <div style={{height: 24, marginTop: 20 }}>而在这里，没有人认识你，没有抬杠，没有点赞，只为你倾诉</div>
-                    <div style={{height: 24, marginTop: 20 }}>如果24小时内没有人发言，该网站将禁止发布，只允许浏览，一个月后该网站关闭数据清空，正如我轻轻的来，最后又轻轻的走，挥一挥衣袖，不带走一片云彩</div>
+                    <div style={{ }}>不知从什么时候开始，想聊些心里话，貌似已经找不到没有合适的渠道。找家人担心家人操心；找朋友担心打扰人家；发圈要检查屏蔽某些人；发微博担心有人抬杠；听音乐有感而发也被调侃网抑云</div>
+                    <div style={{ marginTop: 20 }}>而在这里，没有人认识你，没有抬杠，没有点赞，只为你倾诉</div>
+                    <div style={{ marginTop: 20 }}>如果24小时内没有人发言，该网站将禁止发布，只允许浏览，一个月后该网站关闭数据清空，正如我轻轻的来，最后又轻轻的走，挥一挥衣袖，不带走一片云彩</div>
                 </div>
 
                 <div className="messageWraper">
@@ -92,14 +97,14 @@ export default class extends Component {
                         value={this.state.inputValue}
                         onChange={this.changeText}
                         maxLength={200}
-                        style={{ width: '600px', height: '100px', resize: 'none'}}
+                        className="input"
                         placeholder="请大家共同维护我们这一片净土，感谢你的信任"
                     />
                     <div style={{ color: 'gray', alignSelf: 'flex-end', marginTop: 10 }}>{`${this.state.inputValue.length}/300`}</div>
-                    <button onClick={this.sendNewMessage} className="submit">发送</button>
+                    <button onClick={this.sendNewMessage} style={{ marginTop: "20px"}} className="submit">发送</button>
                 </div>
 
-                <div className="email">联系邮箱：wangliguang_ios@163.com</div>
+                <div className="email">wangliguang_ios@163.com</div>
             </div>
         );
     }
